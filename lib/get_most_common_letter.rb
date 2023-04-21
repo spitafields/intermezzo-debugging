67f34211dd300f0bddbd58e6789bd0c87d211982
alessandro spitaleri <alessandrospitaleri5@gmail.com>
2023-04-21 16:05:01 +0200

    debug challenge

---
+++ lib/get_most_common_letter.rb
@@ -0,0 +1,22 @@
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.chars.each do |char|
+    counter[char] += 1
+  end
+  counter.to_a.sort_by { |k, v| v }[0][0]
+end
+
+# Intended output:
+# 
+# > get_most_common_letter("the roof, the roof, the roof is on fire!")
+# => "o"
+
+
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.chars.each do |char|
+    counter[char] += 1
+  end
+  counter.max_by { |k, v| v }[0]
+end
+#get_most_common_letter("the roof, the roof, the roof is on fire!") # => "o"
